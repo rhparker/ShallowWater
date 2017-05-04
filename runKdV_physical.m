5139cfc2dcdc2f5ceb36c1d4d9c926586e3cc583
Ross Parker <rosshparker@gmail.com>
2017-05-04 15:07:58 -0400

    commented out waitbar

--- runKdV_physical.m
+++ runKdV_physical.m
@@ -86,7 +86,7 @@ function [data, time, xnew] = runKdV_physical(x, u, config, iter, save)
 %     % data vector for EF/EB
 %     dataE = data;
 
-    wb = waitbar(0,'please wait...');
+%     wb = waitbar(0,'please wait...');
     
     for iter = [1:total_iter]
         % advection eq, for testing
@@ -119,12 +119,12 @@ function [data, time, xnew] = runKdV_physical(x, u, config, iter, save)
         if mod(iter, save) == 0
             data = [ data unew ];
             time = [ time k*iter ];
-            waitbar(iter / total_iter);
+%             waitbar(iter / total_iter);
         end
         
     end
 
-    close(wb);
+%     close(wb);
     
 %     plot(xnew, uwave, xnew, data(:,1), xnew, data(:,end));
 %     legend('original 2-pulse', 'start (initial perturbation)', 'end (after timestepping');
